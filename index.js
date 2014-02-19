16179b073786c50ad1acb4e0b6265dac98b02caf
Justin Deal <justin.deal@gmail.com>
2014-02-19 14:27:43 -0600

    correctly send port; callback when connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var spawn = require('child_process').spawn;
+var net = require('net');
 
 var DYNAMO_JAR = 'DynamoDBLocal.jar';
 var DYNAMO_DIR = path.resolve(__dirname, './dynamo');
@@ -10,14 +11,18 @@ function optsToArgs(opts) {
 
   var args = [];
   Object.keys(opts).forEach(function (key) {
-    args.push(key);
+    args.push("--" + key);
     args.push(opts[key]);
   });
   return args;
 }
 
-module.exports = function startDynamo(options) {
+module.exports = function startDynamo(options, cb) {
   options = options || {};
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
 
   var args = ['-Djava.library.path=./DynamoDBLocal_lib/', '-jar', DYNAMO_JAR];
   args = args.concat(optsToArgs(options));
@@ -29,6 +34,20 @@ module.exports = function startDynamo(options) {
   process.on('exit', function () {
     child.kill();
   });
+
+  var tryConnect = function (cb) {
+    var client = net.connect({port: options.port || 8000}, cb);
+
+    client.on('error', function (err) {
+      setTimeout(function () {
+        tryConnect(cb);
+      }, 10);
+    });
+  };
+
+  if (typeof cb === 'function') {
+    tryConnect(cb);
+  }
 
   return child;
 };
